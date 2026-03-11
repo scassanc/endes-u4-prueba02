ad0df08c14328a5fb8a2add158600fc1740ae21f
Sergio Casado <sercassan@gmail.com>
2026-03-11 09:01:51 +0100

    Realizar clase 'Comercial'

--- src/dam2526/pruebaed/domain/Comercial.java
+++ src/dam2526/pruebaed/domain/Comercial.java
@@ -1,4 +1,19 @@
 package dam2526.pruebaed.domain;
 
 public class Comercial extends Empleado {
+  private double ventas;
+
+  public Comercial(String dni, String nombre, String apellidos, double sueldoBase, double ventas) {
+    super(dni, nombre, apellidos, sueldoBase);
+    this.ventas = ventas;
+  }
+
+  public void setVentas(double ventas) {
+    this.ventas = ventas;
+  }
+
+  @Override
+  public double getSueldo() {
+    return sueldoBase + ventas * 0.1;
+  }
 }
