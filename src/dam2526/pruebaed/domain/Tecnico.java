4a967c0aa18f6a03539d23086a5d9867ae3c1514
Sergio Casado <sercassan@gmail.com>
2026-03-11 08:58:11 +0100

    Realizar clase 'Tecnico'

--- src/dam2526/pruebaed/domain/Tecnico.java
+++ src/dam2526/pruebaed/domain/Tecnico.java
@@ -1,4 +1,15 @@
 package dam2526.pruebaed.domain;
 
 public class Tecnico extends Empleado {
+  private int categoria;
+
+  public Tecnico(String dni, String nombre, String apellidos, double sueldoBase, int categoria) {
+    super(dni, nombre, apellidos, sueldoBase);
+    this.categoria = categoria;
+  }
+
+  @Override
+  public double getSueldo() {
+    return sueldoBase + categoria * 100;
+  }
 }
