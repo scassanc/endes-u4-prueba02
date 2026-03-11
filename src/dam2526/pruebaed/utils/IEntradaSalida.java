f4ed20ba26b3223eb3abbc050c916651b87daa1e
Sergio Casado <sercassan@gmail.com>
2026-03-11 08:32:38 +0100

    Realizar interfaz 'IEntradaSalida'

--- src/dam2526/pruebaed/utils/IEntradaSalida.java
+++ src/dam2526/pruebaed/utils/IEntradaSalida.java
@@ -1,4 +1,19 @@
 package dam2526.pruebaed.utils;
 
 public interface IEntradaSalida {
+  void imprimir(String texto);
+
+  void imprimirLinea(String texto);
+
+  String leerTexto(String mensaje);
+
+  int leerEntero(String mensaje);
+
+  double leerImporte(String mensaje);
+
+  void mostrarMenu();
+
+  void limpiarPantalla();
+
+  void pausa();
 }
