1668f9765e40b5da7b5bf092d6539cb90f3fa3f4
Sergio Casado <sercassan@gmail.com>
2026-03-11 08:54:00 +0100

    Realizar clase abstracta 'Empleado'

--- src/dam2526/pruebaed/domain/Empleado.java
+++ src/dam2526/pruebaed/domain/Empleado.java
@@ -1,4 +1,33 @@
 package dam2526.pruebaed.domain;
 
 public abstract class Empleado {
+  private String dni;
+  private String nombre;
+  private String apellidos;
+  private double sueldoBase;
+
+  public Empleado(String dni, String nombre, String apellidos, double sueldoBase) {
+    this.dni = dni;
+    this.nombre = nombre;
+    this.apellidos = apellidos;
+    this.sueldoBase = sueldoBase;
+  }
+
+  public String getDni() {
+    return dni;
+  }
+
+  public String getNombre() {
+    return nombre;
+  }
+
+  public String getApellidos() {
+    return apellidos;
+  }
+
+  public double getSueldoBase() {
+    return sueldoBase;
+  }
+
+  public abstract double getSueldo();
 }
