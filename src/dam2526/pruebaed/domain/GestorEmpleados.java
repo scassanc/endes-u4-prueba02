d4c3d021ddee6c96382d195bb964367abae9220e
Sergio Casado <sercassan@gmail.com>
2026-03-11 09:37:39 +0100

    Realizar clase 'GestorEmpleados'

--- src/dam2526/pruebaed/domain/GestorEmpleados.java
+++ src/dam2526/pruebaed/domain/GestorEmpleados.java
@@ -1,4 +1,97 @@
 package dam2526.pruebaed.domain;
 
+import java.util.List;
+
+import dam2526.pruebaed.utils.IEntradaSalida;
+
 public class GestorEmpleados {
+  private Plantilla plantilla;
+  private IEntradaSalida consola;
+
+  public GestorEmpleados(Plantilla plantilla, IEntradaSalida consola) {
+    this.plantilla = plantilla;
+    this.consola = consola;
+  }
+
+  public void ejecutar() {
+    boolean continuar = true;
+
+    while (continuar) {
+      consola.mostrarMenu();
+      int opcion;
+
+      do {
+        opcion = consola.leerEntero("> ");
+      } while (opcion < 1 || opcion > 4);
+
+      switch (opcion) {
+        case 1: {
+          contratarEmpleado();
+          break;
+        }
+        case 2: {
+          listarTodos();
+          break;
+        }
+        case 3: {
+          listarPorFiltro();
+          break;
+        }
+        case 4: {
+          continuar = false;
+          break;
+        }
+      }
+    }
+  }
+
+  private void contratarEmpleado() {
+    consola.imprimirLinea("1 - Tecnico");
+    consola.imprimirLinea("2 - Comercial");
+    int opcion;
+
+    do {
+      opcion = consola.leerEntero("> ");
+    } while (opcion < 1 || opcion > 2);
+
+    String nombre = consola.leerTexto("Nombre: ");
+    String apellidos = consola.leerTexto("Apellidos: ");
+    String dni = consola.leerTexto("DNI: ");
+    double sueldoBase = consola.leerImporte("Sueldo base: ");
+
+    Empleado empleado = switch (opcion) {
+      case 1 -> {
+        int categoria = consola.leerEntero("Categoría: ");
+        yield new Tecnico(dni, nombre, apellidos, sueldoBase, categoria);
+      }
+      case 2 -> {
+        double ventas = consola.leerImporte("Ventas: ");
+        Comercial comercial = new Comercial(dni, nombre, apellidos, sueldoBase);
+        comercial.setVentas(ventas);
+        yield comercial;
+      }
+      default -> null;
+    };
+
+    plantilla.agregarEmpleado(empleado);
+  }
+
+  private void listarTodos() {
+    List<Empleado> empleados = plantilla.getEmpleadosPorNombre("");
+    listarEmpleados(empleados);
+  }
+
+  private void listarPorFiltro() {
+    String filtro = consola.leerTexto("Filtro: ");
+    List<Empleado> empleados = plantilla.getEmpleadosPorNombre(filtro);
+    listarEmpleados(empleados);
+  }
+
+  private void listarEmpleados(List<Empleado> empleados) {
+    for (int indice = 0; indice < empleados.size(); indice++) {
+      Empleado empleado = empleados.get(indice);
+      consola.imprimirLinea(String.format("%d - %s %s: %f €", indice + 1, empleado.getNombre(), empleado.getApellidos(),
+          empleado.getSueldo()));
+    }
+  }
 }
