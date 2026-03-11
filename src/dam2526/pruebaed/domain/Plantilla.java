6d1ecf18e47781eaec5f72d48ec45cb4ce8e339c
Sergio Casado <sercassan@gmail.com>
2026-03-11 09:08:06 +0100

    Realizar clase 'Plantilla'

--- src/dam2526/pruebaed/domain/Plantilla.java
+++ src/dam2526/pruebaed/domain/Plantilla.java
@@ -1,4 +1,28 @@
 package dam2526.pruebaed.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Plantilla {
+  private List<Empleado> empleados;
+
+  public Plantilla() {
+    empleados = new ArrayList<>();
+  }
+
+  public void agregarEmpleado(Empleado empleado) {
+    empleados.add(empleado);
+  }
+
+  public List<Empleado> getEmpleadosPorNombre(String filtroNombre) {
+    List<Empleado> empleadosFiltrados = new ArrayList<>();
+
+    for (Empleado empleado : empleados) {
+      if (empleado.getNombre().contains(filtroNombre) || empleado.getApellidos().contains(filtroNombre)) {
+        empleadosFiltrados.add(empleado);
+      }
+    }
+
+    return empleadosFiltrados;
+  }
 }
