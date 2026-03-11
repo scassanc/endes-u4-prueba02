19e721e5929b59b8477ec5978e3af608016ec6da
Sergio Casado <sercassan@gmail.com>
2026-03-11 09:37:23 +0100

    Eliminar campo ventas del constructor

--- src/dam2526/pruebaed/domain/Comercial.java
+++ src/dam2526/pruebaed/domain/Comercial.java
@@ -3,9 +3,9 @@ package dam2526.pruebaed.domain;
 public class Comercial extends Empleado {
   private double ventas;
 
-  public Comercial(String dni, String nombre, String apellidos, double sueldoBase, double ventas) {
+  public Comercial(String dni, String nombre, String apellidos, double sueldoBase) {
     super(dni, nombre, apellidos, sueldoBase);
-    this.ventas = ventas;
+    ventas = 0;
   }
 
   public void setVentas(double ventas) {
